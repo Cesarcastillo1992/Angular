22a5a5c26cd026180a91ebf3a2d624dc67556014
Cesar Castillo <cesarg.castillol92@gmail.com>
2024-06-24 18:23:30 -0500

    ajustes visuales

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ClienteService } from '../servicios/cliente.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-inicio',
@@ -27,27 +28,23 @@ export class InicioComponent {
     });
   }
   
-  ngOnInit() {
-  
-  }
-
   buscarCliente() {
     let {tipoDocumento, numeroDocumento} = this.form.value;
+    numeroDocumento = numeroDocumento.replace(/\./g, '');
     this.servicioCliente.loadClientes(tipoDocumento, numeroDocumento).subscribe((cliente) =>{
       console.log(cliente);
       if (cliente) {
         localStorage.setItem('clienteData', JSON.stringify(cliente));
         this.router.navigate(['/resumen']);
       } else {
-        alert('Cliente no encontrado');
-      }
-    
+        Swal.fire({
+          icon: 'info',
+          title: 'Cliente no encontrado',
+          text: 'No se encontraron datos del cliente.',
+        });
+      }  
     });
-    
-    
   }
-
-  
 
   formatNumeroDocumento() {
     let {numeroDocumento} = this.form.value;
@@ -58,7 +55,4 @@ export class InicioComponent {
     }
   }
 
-  removeDots(value: string): string {
-    return value.replace(/\./g, '');
-  }
 }
